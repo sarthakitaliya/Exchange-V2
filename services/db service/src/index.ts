5c070aababde2832565730e02ef59da8bf5285fc
Sarthak Italiya <sarthak.italiya2005@gmail.com>
2025-09-04 10:34:16 +0530

    feat(db): update redis stream handling to process closed orders directly

--- services/db service/src/index.ts
+++ services/db service/src/index.ts
@@ -4,13 +4,7 @@ import { insertClosedOrder } from "./dbQueries/order";
 async function startDbService() {
   try {
     while (true) {
-      const r = await redis.xread(
-        "BLOCK",
-        10000,
-        "STREAMS",
-        REDIS.request,
-        "$"
-      );
+      const r = await redis.xread("BLOCK", 10000, "STREAMS", REDIS.closed, "$");
       if (r) {
         let data: Record<string, string> = {};
         for (const [streams, messages] of r as [
@@ -21,21 +15,10 @@ async function startDbService() {
             for (let i = 0; i < fields.length; i += 2) {
               data[fields[i]] = fields[i + 1];
             }
-            if(REDIS.closed === streams) {
-              await insertClosedOrder(data);
-              await redis.xadd(REDIS.ack, "*", "order_id", data.order_id);
-            }
+            await insertClosedOrder(data);
+            await redis.xadd(REDIS.ack, "*", "order_id", data.order_id);
             await redis.xack(streams, REDIS.grp, id);
           }
-          const status = "success";
-          await redis.xadd(
-            REDIS.ack,
-            "*",
-            "ack_id",
-            data.ack_id,
-            "status",
-            status
-          );
         }
       }
     }
